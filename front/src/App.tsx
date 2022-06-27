2febf8a45aebbe9725753d9ddcc4574a4772519e
ryu <ryu000407@gmail.com>
2022-06-28 08:46:53 +0900

    update: init

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,9 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <div>
+      <h1>Hello, TodoList Contract.</h1>
+    </div>
   );
 };
 
