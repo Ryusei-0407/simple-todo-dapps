dae3b3a602d1a41cb0b15b3809ae0b8cb2ad6023
ryu <ryu000407@gmail.com>
2022-06-29 08:53:55 +0900

    update

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,45 +1,79 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { ethers, providers } from 'ethers';
+import { useContext, useEffect, useState } from 'react';
+import artifact from './abi/TodoList.json';
 
-function App() {
-  const [count, setCount] = useState(0)
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+type Task = {
+  id: string;
+  content: string;
+  isCompleted: boolean;
+};
+
+const useContent = (Contract: ethers.Contract) => {
+  const { taskCount, tasks } = Contract.functions;
+  const [taskCountValue, setTaskCountValue] = useState<string>('');
+  const [tasksValue, setTasksValue] = useState<Array<Task>>([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const _taskCount = await taskCount();
+      setTaskCountValue(_taskCount);
+
+      const _tasks = [];
+      for (let i = 1; i <= _taskCount; ++i) {
+        const _task = await tasks(i);
+        _tasks.push({ ..._task, id: i });
+      }
+      setTasksValue(_tasks);
+    };
+    getTasks();
+  }, []);
+
+  return {
+    taskCount: taskCountValue,
+    tasks: tasksValue,
+  };
+};
+
+const Content = ({ contract }) => {
+  const { taskCount, tasks } = useContent(contract);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <p>{`taskCount ... ${taskCount}`}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Content</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((t, index) => (
+            <tr key={`task.${index}`}>
+              <td>{t.id}</td>
+              <td>{t.content}</td>
+              <td>{t.isCompleted ? 'Completed' : 'Not Completed'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  const provider = new ethers.providers.JsonRpcProvider();
+  const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
+
+  return (
+    <div>
+      <h1>Hello, World</h1>
+      <Content contract={contract} />
+    </div>
+  );
+};
+
+export default App;
